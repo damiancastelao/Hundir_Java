c2aed3f417adcf5954496a9238f44b2d74c06f77
Damian <damian@danielcastelao.org>
2026-04-21 14:02:24 +0200

    creada clase Juego

---
+++ src/Juego.java
@@ -0,0 +1,14 @@
+/**
+ * Clase que maneja eljuego principal
+ */
+public class Juego {
+
+    /**
+     * Crea e inicializa todas las naves del juego.
+     * Coloca las naves en el tablero en
+     * posiciones predefinidas
+     */
+    public void inicializar_naves(){
+        System.out.println("Inicializando naves");
+    }
+}
