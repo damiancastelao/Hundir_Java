ed4696548cbc977f6a268ce217ed10ee4dcc43e7
Damián Nogueiras Meléndez <damian@danielcastelao.org>
2026-05-12 13:47:18 +0200

    Juego (#1)
    * creada clase Juego
    
    * creado constructor de la clase Juego
    
    * creado constructor de la clase Tablero
    
    * JavaDoc

---
+++ src/Juego.java
@@ -0,0 +1,27 @@
+/**
+ * Clase que maneja el juego principal
+ */
+public class Juego {
+
+    /**
+     * Constructor de la clase
+     */
+    public Juego(){
+        inicializar_naves();
+    }
+
+    /**
+     * Crea e inicializa todas las naves del juego.
+     * Coloca las naves en el tablero en
+     * posiciones predefinidas
+     */
+    public void inicializar_naves(){
+        // System.out.println("Inicializando naves");
+        // crear objeto Tablero
+        Tablero miTablero = new Tablero("principal",5);
+        System.out.println(miTablero.casillero[1][1]);
+
+        Tablero otroTablero = new Tablero();
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hundir La FLota");
+        System.out.println("Hundir La FLota");
 
+        Juego miPartida = new Juego();
 
     }
 }
